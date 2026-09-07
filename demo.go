0a981d8338ed193521d2767d659081a17d2bfd3e
my200020002 <158255535+my200020002@users.noreply.github.com>
2026-09-07 19:10:16 +0800

    Update demo.go

--- demo.go
+++ demo.go
@@ -1,77 +1,22 @@
-// Package plugindemo a demo plugin.
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"os"
-	"text/template"
-
+	"net"
+	"strings"
 	"github.com/http-wasm/http-wasm-guest-tinygo/handler"
 	"github.com/http-wasm/http-wasm-guest-tinygo/handler/api"
 )
 
-func init() {
-	var config Config
-	err := json.Unmarshal(handler.Host.GetConfig(), &config)
-	if err != nil {
-		handler.Host.Log(api.LogLevelError, fmt.Sprintf("Could not load config %v", err))
-		os.Exit(1)
-	}
-
-	mw, err := New(config)
-	if err != nil {
-		handler.Host.Log(api.LogLevelError, fmt.Sprintf("Could not load config %v", err))
-		os.Exit(1)
-	}
-	handler.HandleRequestFn = mw.handleRequest
+func main() {
+	handler.HandleRequestFn = handleRequest
 }
-
-// Config the plugin configuration.
-type Config struct {
-	Headers map[string]string `json:"headers,omitempty"`
-}
-
-// Demo a Demo plugin.
-type Demo struct {
-	headers  map[string]string
-	template *template.Template
-}
-
-// New created a new Demo plugin.
-func New(config Config) (*Demo, error) {
-	if len(config.Headers) == 0 {
-		return nil, fmt.Errorf("headers cannot be empty")
+func handleRequest(req api.Request, resp api.Response) (next bool, reqCtx uint32) {
+	addr := req.GetSourceAddr()
+	_, port, err := net.SplitHostPort(addr)
+	if err != nil || port == "" {
+		req.Headers().Set("X-Real-Port", "0")
+		return true, 0
 	}
-
-	return &Demo{
-		headers:  config.Headers,
-		template: template.New("demo").Delims("[[", "]]"),
-	}, nil
-}
-
-func (a *Demo) handleRequest(req api.Request, resp api.Response) (next bool, reqCtx uint32) {
-	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			resp.SetStatusCode(http.StatusInternalServerError)
-			resp.Body().Write([]byte(err.Error()))
-			return false, 0
-		}
-
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			resp.SetStatusCode(http.StatusInternalServerError)
-			resp.Body().Write([]byte(err.Error()))
-			return false, 0
-		}
-
-		req.Headers().Set(key, writer.String())
-	}
-
+	req.Headers().Set("X-Real-Port", port)
 	return true, 0
 }
