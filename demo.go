06a3e6a403420970fa28703d41a39dde789dec3f
my200020002 <158255535+my200020002@users.noreply.github.com>
2026-09-07 19:21:19 +0800

    Update demo.go

--- demo.go
+++ demo.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net"
+
 	"github.com/http-wasm/http-wasm-guest-tinygo/handler"
 	"github.com/http-wasm/http-wasm-guest-tinygo/handler/api"
 )
@@ -9,13 +10,16 @@ import (
 func main() {
 	handler.HandleRequestFn = handleRequest
 }
+
 func handleRequest(req api.Request, resp api.Response) (next bool, reqCtx uint32) {
 	addr := req.GetSourceAddr()
+
 	_, port, err := net.SplitHostPort(addr)
 	if err != nil || port == "" {
 		req.Headers().Set("X-Real-Port", "0")
 		return true, 0
 	}
+
 	req.Headers().Set("X-Real-Port", port)
 	return true, 0
 }
