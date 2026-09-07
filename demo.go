e297810defa2b545409adc9b0e404533068a8d49
my200020002 <158255535+my200020002@users.noreply.github.com>
2026-09-07 19:35:31 +0800

    Update demo.go

--- demo.go
+++ demo.go
@@ -12,8 +12,9 @@ func main() {
 }
 
 func handleRequest(req api.Request, resp api.Response) (next bool, reqCtx uint32) {
+	req.Headers().Set("X-Wasm-Debug", "triggered")
 	addr := req.GetSourceAddr()
-
+    req.Headers().Set("X-Debug-Source-Addr", addr)
 	_, port, err := net.SplitHostPort(addr)
 	if err != nil || port == "" {
 		req.Headers().Set("X-Real-Port", "0")
